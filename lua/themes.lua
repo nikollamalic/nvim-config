b1f0b64bbd7bb9fd39784ae6dde350efd0dc857e
Nikola <nikola@getaurox.com>
2022-09-16 12:23:28 +0200

    Add theme

--- lua/themes.lua
+++ lua/themes.lua
@@ -1,12 +1,20 @@
-local current_theme = "everforest";
+local current_theme = "gruvbox-material";
 local variant = "dark"
 
+vim.o.background = variant
+vim.o.termguicolors = true
+
 if (current_theme == "everforest") then
-  vim.o.background = variant
-  vim.o.termguicolors = true
   vim.g.everforest_background = "medium"
   vim.g.everforest_transparent_background = 0
   vim.g.everforest_diagnostic_text_highlight = 1
 
   vim.cmd[[colorscheme everforest]]
+end
+
+if (current_theme == "gruvbox-material") then
+  vim.g.gruvbox_material_background = 'soft' -- hard, medium, soft
+  vim.g.gruvbox_material_better_performance = 1
+
+  vim.cmd[[colorscheme gruvbox-material]]
 end
