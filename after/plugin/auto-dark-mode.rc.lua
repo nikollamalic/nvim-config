5e62689036d49760560e51e29a521320739902cc
Nikola <nikola@getaurox.com>
2022-12-04 19:29:30 +0100

    Dark theme switcher

--- after/plugin/auto-dark-mode.rc.lua
+++ after/plugin/auto-dark-mode.rc.lua
@@ -3,9 +3,12 @@ local status, theme_switcher = pcall(require, "auto-dark-mode")
 if (not status) then return end
 
 local set_dark_mode = function()
+  -- nightfox, duskfox, nordfox, terafox, carbonfox
+  -- vim.cmd [[colorscheme dawnfox]]
 end
 
 local set_light_mode = function()
+  -- dayfox, dawnfox, darcula
   -- vim.cmd [[colorscheme dawnfox]]
 end
 
--- lua/themes.lua
+++ lua/themes.lua
@@ -1,12 +1,4 @@
-
--- vim.g.gruvbox_material_background = 'hard' -- hard, medium, soft
--- vim.g.gruvbox_material_better_performance = 1
-
 -- vim.cmd [[colorscheme darcula]]
-
--- vim.cmd [[colorscheme neosolarized]]
 
 -- [dayfox, dawnfox, nightfox, duskfox, nordfox, terafox, carbonfox]
 -- vim.cmd [[colorscheme carbonfox]]
-
--- vim.cmd [[colorscheme melange]]
